fb2c2e79973b7d8e78f0451922a6be13e2e4d100
Rubén <74114026+rubzip@users.noreply.github.com>
2026-03-12 14:07:53 +0100

    Update tuits.py

--- backend/app/router/tuits.py
+++ backend/app/router/tuits.py
@@ -1,1 +1,29 @@
+from fastapi import APIRouter, Depends
+from models.base import TweetBase
+from models.relations import Like
 
+
+router = APIRouter(
+    prefix="/tuits",
+    tags=["Tuits"]
+)
+
+@router.post("/", response_model=Tuit, status_code=status.HTTP_201_CREATED)
+async def create_tuit(tuit: TuitCreate):
+    """Creates a new tuit."""
+    pass
+
+@router.post("/{parent_id}/respond", response_model=Tuit)
+async def respond_tuit(parent_id: UUID, tuit: TuitCreate):
+    """Creates tuit as response of other."""
+    pass
+
+@router.delete("/{tuit_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_tuit(tuit_id: UUID):
+    """Delete specific tuit by id"""
+    pass
+
+@router.get("/{tuit_id}", response_model=Tuit)
+async def get_tuit(tuit_id: UUID):
+    """Gets specific tuit."""
+    pass
